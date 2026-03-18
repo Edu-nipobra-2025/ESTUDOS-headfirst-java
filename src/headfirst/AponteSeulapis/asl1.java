667f833d04265d700fb6bb6e6e3cce97190bfdca
Edu-nipobra-2025 <riudyeduardo@gmail.com>
2026-03-18 11:47:24 -0300

    a

--- src/headfirst/AponteSeulapis/asl1.java
+++ src/headfirst/AponteSeulapis/asl1.java
@@ -2,11 +2,29 @@ package headfirst.AponteSeulapis;
 
 public class asl1 {
     int size = 27; // var size de tamanho 27
-String name = "Fido"; //String name que retorna Fido
-Dog myDog = new Dog(); //Objeto myDog apontando pra Dog
-    x = size - 5 ; //var X igual a size - 5
-    if (x < 15) myDog.bark(8); //se x menor que quinze, o myDog late 8 vezes.
-    while (x > 3) { //enquanto x maior que três
+    String name = "Fido"; //String name que retorna Fido
+    Dog myDog = new Dog(); //Objeto myDog apontando pra Dog
+    x =size -5; //var X igual a size - 5
+    if(x< 15)myDog.bark(8); //se x menor que quinze, o myDog late 8 vezes.
+    while(x >3)
+
+    { //enquanto x maior que três
         myDog.play(); //o cachorro brinca
     }
+
+    int[] numList = {2, 4, 6, 8};
+    System.out.print("Hello");
+     System.out.print("Dog: "+name);
+    String num = "8";
+    int z = Integer.parseInt(num);
+    //Roda um vetor com quatro numeros pares, printa um hello dog: (nome)
+    //cria uma variável String num de valor "Oito", e cria um int z
+    //que torna a String em número
+}
+try{
+readTheFile("myFile.txt");
+    }
+catch(FileFoundException ex){
+    System.out.print("File not found.");
+    //tenta ler um arquivo myFile.txt, se não tiver, retornará file not found.
 }
---
+++ src/headfirst/Paralelo/MyfirstApp.java
@@ -0,0 +1,8 @@
+package headfirst.Paralelo;
+
+public class MyfirstApp {
+    public static void main(String[] args){
+        System.out.println("I rule!");
+        System.out.println("The World");
+    }
+}
